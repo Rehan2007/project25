34a7dfa43fc965f326fb4195940c67963c8ab166
Rehan2007 <73646262+Rehan2007@users.noreply.github.com>
2021-04-21 15:13:39 +0530

    Add files via upload

---
+++ project24/MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,36 @@
+var wall,wall2,wall3
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+paper1=CreateSprite
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
---
+++ project24/dustbin.js
@@ -0,0 +1,8 @@
+function Dustbin(){
+wall=createSprite(340,610,20,100)
+wall.shapeColor=rgb(255,0,0);
+wall2=createSprite(430,649,200,20)
+wall2.shapeColor=rgb(255,0,0);
+wall3=createSprite(540,610,20,100)
+wall3.shapeColor=rgb(255,0,0);
+}
